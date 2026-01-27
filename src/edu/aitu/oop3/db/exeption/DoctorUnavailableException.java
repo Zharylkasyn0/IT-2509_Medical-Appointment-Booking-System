6caf1f4c9ea9c0742f017665896512c1ade8fe0e
Zharylkasyn0 <_email@___github.com>
2026-01-27 10:28:16 +0500

    some mistakes fixed

--- src/edu/aitu/oop3/db/exeption/DoctorUnavailableException.java
+++
@@ -1,7 +0,0 @@
-package edu.aitu.oop3.db.exeption;
-
-public class DoctorUnavailableException extends AppointmentException {
-    public DoctorUnavailableException() {
-        super("The doctor is not available at this time.");
-    }
-}
